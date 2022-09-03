9cdb94e2d1f78b65ada28519c8b43e294a79b570
AhmedFady775 <fadiahmed298@gmail.com>
2022-09-03 21:05:31 +0200

    1

--- models/posts/postModel.js
+++ models/posts/postModel.js
@@ -2,14 +2,14 @@ import mongoose from "mongoose";
 
 const postSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true },
-    date: { type: String, required: true },
-    image: { type: String, required: true },
+    name: { type: String },
+    date: { type: String },
+    image: { type: String },
     images: [String],
     videos: [String],
-    description: { type: String, required: true },
-    subject: { type: String, required: true },
-    status: { type: Boolean, required: true },
+    description: { type: String },
+    subject: { type: String },
+    status: { type: Boolean },
   },
   {
     timestamps: true,
