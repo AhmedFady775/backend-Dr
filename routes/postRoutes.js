383465c3c051440fe6071b888d6fb0745018d076
AhmedFady775 <fadiahmed298@gmail.com>
2022-09-15 17:07:47 +0200

    1

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,5 +1,7 @@
 import express from "express";
 import Post from "../models/posts/postModel.js";
+import expressAsyncHandler from "express-async-handler";
+import { isAuth } from "../utils.js";
 
 const postRouter = express.Router();
 
@@ -17,47 +19,59 @@ postRouter.get("/:id", async (req, res) => {
   }
 });
 
-postRouter.post("/createPost", async (req, res) => {
-  const newPost = new Post({
-    name: req.body.name,
-    date: req.body.date,
-    image: req.body.image,
-    images: req.body.images,
-    videos: req.body.videos,
-    description: req.body.description,
-    subject: req.body.subject,
-    status: req.body.status,
-  });
-  const post = await newPost.save();
-  res.send({ message: "Post Created", post });
-});
+postRouter.post(
+  "/createPost",
+  isAuth,
+  expressAsyncHandler(async (req, res) => {
+    const newPost = new Post({
+      name: req.body.name,
+      date: req.body.date,
+      image: req.body.image,
+      images: req.body.images,
+      videos: req.body.videos,
+      description: req.body.description,
+      subject: req.body.subject,
+      status: req.body.status,
+    });
+    const post = await newPost.save();
+    res.send({ message: "Post Created", post });
+  })
+);
 
-postRouter.delete("/delete/:id", async (req, res) => {
-  const post = await Post.findById(req.params.id);
-  if (post) {
-    await post.remove();
-    res.send({ message: "Post Deleted" });
-  } else {
-    res.status(404).send({ message: "Post Not Found" });
-  }
-});
+postRouter.delete(
+  "/delete/:id",
+  isAuth,
+  expressAsyncHandler(async (req, res) => {
+    const post = await Post.findById(req.params.id);
+    if (post) {
+      await post.remove();
+      res.send({ message: "Post Deleted" });
+    } else {
+      res.status(404).send({ message: "Post Not Found" });
+    }
+  })
+);
 
-postRouter.put("/update/:id", async (req, res) => {
-  const post = await Post.findById(req.params.id);
-  if (post) {
-    post.name = req.body.name;
-    post.date = req.body.date;
-    post.image = req.body.image;
-    post.images = req.body.images;
-    post.videos = req.body.videos;
-    post.description = req.body.description;
-    post.subject = req.body.subject;
-    post.status = req.body.status;
-    await post.save();
-    res.send({ message: "Post Updated" });
-  } else {
-    res.status(404).send({ message: "Post Not Found" });
-  }
-});
+postRouter.put(
+  "/update/:id",
+  isAuth,
+  expressAsyncHandler(async (req, res) => {
+    const post = await Post.findById(req.params.id);
+    if (post) {
+      post.name = req.body.name;
+      post.date = req.body.date;
+      post.image = req.body.image;
+      post.images = req.body.images;
+      post.videos = req.body.videos;
+      post.description = req.body.description;
+      post.subject = req.body.subject;
+      post.status = req.body.status;
+      await post.save();
+      res.send({ message: "Post Updated" });
+    } else {
+      res.status(404).send({ message: "Post Not Found" });
+    }
+  })
+);
 
 export default postRouter;
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import seedRouter from "./routes/seedRoutes.js";
 import userRouter from "./routes/userRoutes.js";
 import postRouter from "./routes/postRoutes.js";
 import uploadRouter from "./routes/uploadRoutes.js";
@@ -19,23 +18,30 @@ mongoose
   });
 
 const app = express();
-app.use(cors());
-app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Headers, *, Access-Control-Allow-Origin",
-    "Origin, X-Requested-with, Content_Type,Accept,Authorization",
-    "http://localhost:4200"
-  );
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "PUT,POST,PATCH,DELETE,GET");
-    return res.status(200).json({});
-  }
-  next();
-});
+app.use(
+  cors(
+    {
+      methods: ["PUT,POST,PATCH,DELETE,GET,OPTIONS"],
+    },
+    {
+      origin:
+        "Access-Control-Allow-Methods, *, Access-Control-Allow-Origin, Origin, X-Requested-with, Content_Type,Accept,Authorization",
+    }
+  )
+);
+// app.use((req, res, next) => {
+//   if (req.method === "PUT,POST,PATCH,DELETE,GET,OPTIONS") {
+//     res.header(
+//       "Access-Control-Allow-Methods, *, Access-Control-Allow-Origin",
+//       "Origin, X-Requested-with, Content_Type,Accept,Authorization"
+//     );
+//     return res.status(200).json({});
+//   }
+//   next();
+// });
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // test
-app.use("/api/seed", seedRouter);
 app.use("/api/users", userRouter);
 app.use("/api/posts", postRouter);
 app.use("/api/upload", uploadRouter);
